26382df1d50eac2a7b1088b2be3cd5bb0648eda1
Roman Leshin <vorzee@gmail.com>
2024-08-11 17:05:23 +0500

    Реализовано приложение для расчета бонусных миль за купленный авиабилет.
    Рассчитаны мили на основе стоимости билета с использованием заданного коэффициента, результат расчета выводится на экран.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -12,31 +10,11 @@ public class Main {
         // значения заведённых переменных. Ответ сохраняете в
         // новую переменную и выводите на экран
 
-        // Вариант 1:
         int amount = 5389;
         int ratioBonus = 20;
 
         int miles = amount / ratioBonus;
 
         System.out.println("Начислено бонусных миль: " + miles);
-
-        // Вариант 2:
-        int amount2 = 433436;
-        double ratioBonus2 = 0.05;
-
-        int miles2 = (int) (amount2 * ratioBonus2);
-
-        System.out.println("Начислено бонусных миль: " + miles2);
-
-        // Вариант 3:
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите сумму билетов: ");
-        int amount3 = (int) scanner.nextDouble();
-        double ratioBonus3 = 0.05;
-        scanner.close();
-
-        int miles3 = (int) (amount3 * ratioBonus3);
-
-        System.out.println("Начислено бонусных миль: " + miles3);
     }
 }
